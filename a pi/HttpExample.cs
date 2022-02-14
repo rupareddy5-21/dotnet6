fd876cb3be9b9d1ba952b7105c516d859cd717ef
Rupa Reddy <rupareddy@microsoft.com>
2022-02-14 12:52:29 +0530

    Add files via upload

---
+++ a pi/HttpExample.cs
@@ -0,0 +1,27 @@
+namespace dotnet_60
+{
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.Extensions.Logging;
+
+    public static class HttpExample
+    {
+        [FunctionName("HttpExample")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+string date = "2021-11-24T06:15:25.964Z";
+
+            return new OkObjectResult(date + "one two three four five six");
+        }
+    }
+}
